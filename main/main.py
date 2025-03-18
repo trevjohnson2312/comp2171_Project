f7f604a871fe739f3e1db7cd5129b628202f5728
Damario Escoffery <damario7324@gmail.com>
2025-03-18 11:37:20 -0500

    Updated

--- main/main.py
+++ main/main.py
@@ -1,27 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import re
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 from datetime import datetime
-from flask import flash
+from extensions import db
  
 app = Flask(__name__)
  
 app.secret_key = 'your secret key'
  
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'comp2170'
-app.config['MYSQL_DB'] = 'school_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/school_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
-mysql = MySQL(app)
+db.init_app(app)
 
-def log_operation(student_id, operation):
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    query = '''INSERT INTO school_db.student_audit (student_id, operation, changed_at, changed_by) VALUES (%s, %s, %s, %s)'''
-    cursor.execute(query, (student_id, operation, datetime.now(), session['role']))
-    mysql.connection.commit()
-    cursor.close()
+from models import Users, StudentAttendance, Students, StudentAudit, ParentsContact
+
+def log_operation(student_id: int, operation: str):
+    audit_log = StudentAudit(
+        student_id = student_id,
+        operation = operation,
+        changed_at = datetime.now(),
+        changed_by = session.get('role', 'Unknown')
+    )
+
+    db.seesion.add(audit_log)
+    db.session.commit()
  
 @app.route('/')
 @app.route('/login', methods =['GET', 'POST'])
@@ -30,65 +33,109 @@ def login():
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM users WHERE username = % s AND password = % s', (username, password, ))
-        users = cursor.fetchone()
-        if users:
+        user = Users.query.filter_by(username = username, password = password).first()
+        if user:
             session['loggedin'] = True
-            session['id'] = users['id']
-            session['username'] = users['username']
-            session['role'] = users['role']
+            session['id'] = user.id
+            session['username'] = user.username
+            session['role'] = user.role
 
-            if users['role'] == 'Teacher':
-                return redirect(url_for('teacher_dash'))
-            
-            elif users['role'] == 'Principal':
-                return redirect(url_for('principal_dash'))
-            
-            elif users['role'] == 'Dean':
-                return redirect(url_for('dean_dash'))
-            
+            if user.role == 'Teacher':
+                msg = 'Logged in successfully as Teacher!'
+                return redirect(url_for('index_teach'))
+            elif user.role == 'Principal':
+                msg = 'Logged in successfully as Principal!'
+                return redirect(url_for('index_principal'))
+            elif user.role == 'Dean':
+                msg = 'Logged in successfully as Dean!'
+                return redirect(url_for('index_dean'))
             else:
-                return redirect(url_for('presec_dash'))
+                msg = 'Logged in successfully as Prefect/Security!'
+                return redirect(url_for('index_presec'))
         else:
             msg = 'Incorrect username/password!'
     return render_template('login.html', msg = msg)
 
-@app.route('/principal_dash')
-def principal_dash():
-    if 'loggedin' in session and session['role'] == 'Principal':
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-
-        cursor.execute('SELECT student_id, operation, changed_at, changed_by FROM school_db.student_audit')
-        audit_logs = cursor.fetchall()
-
-        msg = 'Logged in successfully as Principal!'
-        return render_template('principal_dash.html', msg=msg, audit_logs=audit_logs)
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
     return redirect(url_for('login'))
 
+# Routes for Teacher
 @app.route('/teacher_dash')
-def teacher_dash():
+def index_teach():
     if 'loggedin' in session and session['role'] == 'Teacher':
-
         return render_template('teacher_dash.html')
     return redirect(url_for('login'))
 
-@app.route('/dean_dash')
-def dean_dash():
-    if 'loggedin' in session and session['role'] == 'Dean':
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+@app.route('/teacher_dash/edit_attendance')
+def index_edit_atten():
+    return render_template('edit_attendance.html')
 
-        cursor.execute('SELECT student_id, operation, changed_at, chnaged_by FROM school_db.student_audit')
-        audit_logs = cursor.fetchall()
+@app.route('/teacher_dash/view_atten_hist')
+def index_view_atten():
+    return render_template('view_attend_hist.html')
 
-        return render_template('dean_dash.html',audit_logs=audit_logs)
+@app.route('/fetch_students', methods = ['GET'])
+def get_students():
+    try:
+        students = Students.query.all()
+        student_list = [{'id': student.id, 'name': student.name} for student in students]
+        return jsonify(student_list), 200
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)}), 500
+
+@app.route('/sbmt_attendance', methods = ['POST'])
+def submit_attendance():
+    try:
+        data = request.get_json()
+
+        if not data or not isinstance(data, dict):
+            return jsonify({'success': False, 'error': 'Invalid input data'}), 400
+
+        for student_id, status in data.items():
+            student_id = int(student_id)
+
+            student = Students.query.get(student_id)
+
+            if not student:
+                return jsonify({'success': False, 'error': f'Student ID {student_id} not found'}), 404
+
+            attendance = StudentAttendance.query.filter_by(student_id = student_id, date = datetime.utcnow().date()).first()
+
+            if attendance:
+                attendance.status = status
+            else:
+                new_attendance = StudentAttendance(student_id = student_id, status = status, date = db.func.current_date())
+                db.session.add(new_attendance)
+
+        db.session.commit()
+        return jsonify({"success": True, "message": "Attendance Updated Successfully!!"})
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"success": False, "error": str(e)}), 500
+
+@app.route('/back_teacher')
+def back_teacher():
+    return redirect(url_for('index_teach'))
+
+
+# Routes for Principal and Dean
+@app.route('/principal_dash')
+def index_principal():
+    if 'loggedin' in session and session['role'] == 'Principal':
+        audit_logs = StudentAudit.query.all()
+        msg = 'Logged in successfully as Principal!'
+        return render_template('principal_dash.html', msg = msg, audit_logs = audit_logs)
     return redirect(url_for('login'))
 
-@app.route('/presec_dash')
-def presec_dash():
-    if 'loggedin' in session and (session['role'] == 'Prefect' or session['role'] == 'Security'):
-
-        return render_template('presec_dash.html')
+@app.route('/dean_dash')
+def index_dean():
+    if 'loggedin' in session and session['role'] == 'Dean':
+        audit_logs = StudentAudit.query.all()
+        return render_template('dean_dash.html',audit_logs = audit_logs)
     return redirect(url_for('login'))
 
 @app.route('/edit_registry')
@@ -97,140 +144,138 @@ def edit_registry():
         return render_template('edit_registry.html')
     return redirect(url_for('login'))
     
-
-
-
-
-
-
-
 @app.route('/edit_registry/add_student', methods=['GET'])
 def add_student():
     if 'loggedin' in session and (session['role'] == 'Dean' or session['role'] == 'Principal'):
         return render_template('student_add.html')
-    return redirect(url_for('login'))
-    
-
+    return redirect(url_for('login'))    
     
 @app.route('/edit_registry/add_student', methods=['POST'])
 def add_student_update():
-    student_id = request.form['student_id']
-    st_first_name = request.form['st_first_name']
-    st_last_name = request.form['st_last_name']
-    grade = request.form['grade']
-    parent_first_name = request.form['parent_first_name']
-    parent_last_name = request.form['parent_last_name']
-    parent_email = request.form['parent_email']
-    parent_telephone = request.form['parent_telephone']
+    try:
+        student_id = request.form['student_id']
+        st_first_name = request.form['st_first_name']
+        st_last_name = request.form['st_last_name']
+        grade = request.form['grade']
+        parent_first_name = request.form['parent_first_name']
+        parent_last_name = request.form['parent_last_name']
+        parent_email = request.form['parent_email']
+        parent_telephone = request.form['parent_telephone']
 
-    student_name = f"{st_first_name} {st_last_name}"
-    parent_name = f"{parent_first_name} {parent_last_name}"
+        student_name = f"{st_first_name} {st_last_name}"
+        parent_name = f"{parent_first_name} {parent_last_name}"
+
+        new_student = Students(
+            id = student_id,
+            name = student_name,
+            grade = grade
+        )
+
+        new_parent = ParentsContact(
+            student_id = student_id,
+            parent_name = parent_name,
+            email = parent_email,
+            telephone_number = parent_telephone
+        )
+
+        db.session.add(new_student)
+        db.session.add(new_parent)
+        db.session.commit
+
+        log_operation(student_id, 'Added')
+        return redirect(url_for('add_student'))
+    except IntegrityError:    
+        db.session.rollback()
+        return "Student ID already exists!", 400
 
 
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-
-    p_query = '''INSERT INTO school_db.parents_contact (student_id, parent_name, email, telephone_number) VALUES (%s, %s, %s, %s)'''
-    cursor.execute(p_query, (student_id, parent_name, parent_email, parent_telephone))
-    mysql.connection.commit()
-
-    s_query = '''INSERT INTO school_db.students (id, name, grade) VALUES (%s, %s, %s)'''
-    cursor.execute(s_query, (student_id, student_name, grade))
-    mysql.connection.commit()
-
-    cursor.close()
-
-    log_operation(student_id, 'Added')
-
-    return redirect(url_for('add_student'))
-
-
-
-@app.route('/edit_registry/edit_student', methods=['GET'])
+@app.route('/edit_registry/edit_student', methods = ['GET'])
 def edit_student():
     if 'loggedin' in session and (session['role'] == 'Dean' or session['role'] == 'Principal'):
         student_id = request.args.get('student_id')
         
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        student = Students.query.get(student_id)
+        parent = ParentsContact.query.get(student_id)
 
-        cursor.execute('SELECT * FROM school_db.students WHERE id = %s', (student_id,))
-        student = cursor.fetchone()
-
-        cursor.execute('SELECT * FROM school_db.parents_contact WHERE student_id = %s', (student_id,))
-        parent = cursor.fetchone()
-
-        return render_template('student_edit.html', student=student, parent=parent)
-
-        
+        if not student or not parent:
+            return "Student not found!", 404
+        return render_template('student_edit.html', student = student, parent = parent)
     return redirect(url_for('login'))
     
     
-@app.route('/edit_registry/edit_student', methods=['POST'])
+@app.route('/edit_registry/edit_student', methods = ['POST'])
 def edit_student_update():
-    student_id = request.form['student_id']
-    st_first_name = request.form['st_first_name']
-    st_last_name = request.form['st_last_name']
-    grade = request.form['grade']
-    parent_first_name = request.form['parent_first_name']
-    parent_last_name = request.form['parent_last_name']
-    parent_email = request.form['parent_email']
-    parent_telephone = request.form['parent_telephone']
+    try:
+        student_id = request.form['student_id']
+        st_first_name = request.form['st_first_name']
+        st_last_name = request.form['st_last_name']
+        grade = request.form['grade']
+        parent_first_name = request.form['parent_first_name']
+        parent_last_name = request.form['parent_last_name']
+        parent_email = request.form['parent_email']
+        parent_telephone = request.form['parent_telephone']
 
-    student_name = f"{st_first_name} {st_last_name}"
-    parent_name = f"{parent_first_name} {parent_last_name}"
+        student_name = f"{st_first_name} {st_last_name}"
+        parent_name = f"{parent_first_name} {parent_last_name}"
 
+        student = Students.query.get(student_id)
+        parent = ParentsContact.query.get(student_id)
 
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        if student and parent:
+            student.name = student_name
+            student.grade = grade
+            parent.parent_name = parent_name
+            parent.email = parent_email
+            parent.telephone_number = parent_telephone
 
-    p_query = '''INSERT INTO school_db.parents_contact (student_id, parent_name, email, telephone_number) VALUES (%s, %s, %s, %s)'''
-    cursor.execute(p_query, (student_id, parent_name, parent_email, parent_telephone))
-    mysql.connection.commit()
-
-    s_query = '''INSERT INTO school_db.students (id, name, grade) VALUES (%s, %s, %s)'''
-    cursor.execute(s_query, (student_id, student_name, grade))
-    mysql.connection.commit()
-
-    cursor.close()
-
-    log_operation(student_id, 'Added')
-
-    return redirect(url_for('edit_student'))
+            db.session.commit()
+            log_operation(student_id, 'Added')
+        return redirect(url_for('edit_student'))
+    except Exception as e:
+        db.session.rollback()
+        return f"An error occurred: {str(e)}", 500
     
-
 
 @app.route('/edit_registry/remove_student', methods=['GET'])
 def remove_student():
     if 'loggedin' in session and (session['role'] == 'Dean' or session['role'] == 'Principal'):
         return render_template('student_remove.html')
+    return redirect(url_for('login'))
 
 
-@app.route('/edit_registry/remove_student', methods=['POST'])
+@app.route('/edit_registry/remove_student', methods = ['POST'])
 def remove_student_update():
-    student_id = request.form['student_id']
+    try:
+        student_id = request.form['student_id']
 
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        student = Students.query.get(student_id)
+        parent = ParentsContact.query.get(student_id)
 
-    s_query = '''DELETE FROM school_db.students WHERE id = %s'''
-    cursor.execute(s_query, (student_id,))
-    mysql.connection.commit()
+        if student and parent:
+            db.session.delete(student)
+            db.session.delete(parent)
+            db.session.commit()
 
-    p_query = '''DELETE FROM school_db.parents_contact WHERE student_id = %s'''
-    cursor.execute(p_query, (student_id,))
-    mysql.connection.commit()
+            log_operation(student_id, 'Removed')
+        return redirect(url_for('remove_student'))
+    except Exception as e:
+        db.session.rollback()
+        return f"An error occurred: str{(e)}", 500
 
-    cursor.close()
+@app.route('/back_principal')
+def back_principal():
+    return redirect(url_for('index_principal'))
 
-    log_operation(student_id, 'Removed')
+@app.route('/back_dean')
+def back_dean():
+    return redirect(url_for('index_dean'))
 
-    return redirect(url_for('remove_student'))
-    
-
- 
-@app.route('/logout')
-def logout():
-    session.pop('loggedin', None)
-    session.pop('id', None)
-    session.pop('username', None)
+# Routes for PreSec
+@app.route('/presec_dash')
+def index_presec():
+    if 'loggedin' in session and (session['role'] == 'Prefect' or session['role'] == 'Security'):
+        return render_template('presec_dash.html')
     return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host = "0.0.0.0", port = 5001, debug = True)
